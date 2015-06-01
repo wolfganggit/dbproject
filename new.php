f118367a8ae6f04503a1aeabe14ab4f0073b3c23
Andi <andreas.stadler@indiekind.com>
2015-06-01 22:58:24 +0200

    kleines update

--- new.php
+++ new.php
@@ -1,8 +1,27 @@
 <html>
   <head>
-    <title></title>
+    <title>Add new Entries </title>
     <meta content="">
-    <style></style>
+    <link rel="stylesheet" href="dbproject.css" title="dbproject" />
+
+    <style type="text/css">
+		body {background-image: url(./images/upfeathers.png); background-repeat: repeat;}
+		#container {background-color: red; width: 800px; height: 600px;  
+			margin-left: auto; margin-right: auto ;
+			margin-top: 20px; background-image: url(images/upfeathers.png);}
+    </style>
+
   </head>
-  <body></body>
+
+  <body>
+  	<h1 style="text-align: center; margin-top:70px;"> Add a Person </h1>
+
+  	<div id="container">
+  		
+	</div>
+
+
+
+
+  </body>
 </html>
