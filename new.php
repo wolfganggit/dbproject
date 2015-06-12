c41d3ec16c1ad6978194750d55527963430d83fa
Andi <andreas.stadler@indiekind.com>
2015-06-12 17:31:41 +0200

    juhu

--- new.php
+++ new.php
@@ -86,9 +86,24 @@ Sendemethode auf Post ändern
         $erfolgreichEingefuegt = ""; 
         echo $erfolgreichEingefuegt;
 
+
+
+
+          // Erstellen der Liste für die Länderauswahl
+          $countryList = array("Afghanistan","Albania","Algeria","American Samoa","Andorra","Angola","Anguilla","Antarctica","Antigua and Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia and Herzegovina","Botswana","Bouvet Island","Brazil","British Antarctic Territory","British Indian Ocean Territory","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Canton and Enderbury Islands","Cape Verde","Cayman Islands","Central African Republic","Chad","Chile","China","Christmas Island","Cocos [Keeling] Islands","Colombia","Comoros","Congo - Brazzaville","Congo - Kinshasa","Cook Islands","Costa Rica","Croatia","Cuba","Cyprus","Czech Republic","Côte d’Ivoire","Denmark","Djibouti","Dominica","Dominican Republic","Dronning Maud Land","East Germany","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Guiana","French Polynesia","French Southern Territories","French Southern and Antarctic Territories","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guadeloupe","Guam","Guatemala","Guernsey","Guinea","Guinea-Bissau","Guyana","Haiti","Heard Island and McDonald Islands","Honduras","Hong Kong SAR China","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Johnston Island","Jordan","Kazakhstan","Kenya","Kiribati","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau SAR China","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Martinique","Mauritania","Mauritius","Mayotte","Metropolitan France","Mexico","Micronesia","Midway Islands","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Myanmar [Burma]","Namibia","Nauru","Nepal","Netherlands","Netherlands Antilles","Neutral Zone","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Niue","Norfolk Island","North Korea","North Vietnam","Northern Mariana Islands","Norway","Oman","Pacific Islands Trust Territory","Pakistan","Palau","Palestinian Territories","Panama","Panama Canal Zone","Papua New Guinea","Paraguay","People's Democratic Republic of Yemen","Peru","Philippines","Pitcairn Islands","Poland","Portugal","Puerto Rico","Qatar","Romania","Russia","Rwanda","Réunion","Saint Barthélemy","Saint Helena","Saint Kitts and Nevis","Saint Lucia","Saint Martin","Saint Pierre and Miquelon","Saint Vincent and the Grenadines","Samoa","San Marino","Saudi Arabia","Senegal","Serbia","Serbia and Montenegro","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Georgia and the South Sandwich Islands","South Korea","Spain","Sri Lanka","Sudan","Suriname","Svalbard and Jan Mayen","Swaziland","Sweden","Switzerland","Syria","São Tomé and Príncipe","Taiwan","Tajikistan","Tanzania","Thailand","Timor-Leste","Togo","Tokelau","Tonga","Trinidad and Tobago","Tunisia","Turkey","Turkmenistan","Turks and Caicos Islands","Tuvalu","U.S. Minor Outlying Islands","U.S. Miscellaneous Pacific Islands","U.S. Virgin Islands","Uganda","Ukraine","Union of Soviet Socialist Republics","United Arab Emirates","United Kingdom","United States","Unknown or Invalid Region","Uruguay","Uzbekistan","Vanuatu","Vatican City","Venezuela","Vietnam","Wake Island","Wallis and Futuna","Western Sahara","Yemen","Zambia","Zimbabwe","Åland Islands");
+
+          $countryOptions = "";
+
+          // Länderoptionen zusammenbauen
+          foreach($countryList as $cvalue)
+          {
+              $countryOptions .= '<option value="' . $cvalue . '" >' . $cvalue . '</option>';
+          }
+
       ?>
 
       <div id = "border">
+        Please fill all fields.  
       <form method="POST" action="new.php">
 
         <fieldset name = "person" >
@@ -103,7 +118,9 @@ Sendemethode auf Post ändern
                 <strong> Gender: </strong> <br>
                 <input type="radio" id="female" name="gender" value="f" checked="checked"><label for="f"> female </label><br> 
                 <input type="radio" id="male" name="gender" value="m"><label for="m">  male </label><br> 
+                <!--
                 <input type="radio" id="other" name="gender" value="o"><label for="o"> other </label> 
+                -->
               </fieldset>
           </p>
 
@@ -149,7 +166,8 @@ Sendemethode auf Post ändern
           Street Number:  <input name="streetnumber" type="text" size="30" maxlength="40"> <br> <br>
           Town: <input name="town" type="text" size="30" maxlength="40"> <br> <br>
           Postalcode: <input name="postalcode" type="text" size="30" maxlength="40"> <br> <br>
-          Nation: <input name="nation" type="text" size="30" maxlength="40"> <br> <br>
+          <!-- Nation: <input name="nation" type="text" size="30" maxlength="40"> <br> <br> -->
+          Nation: <select id="country" name="nation" id="nation"><?php echo $countryOptions; ?></select>
         </p>
 
         <hr>
@@ -276,7 +294,7 @@ Sendemethode auf Post ändern
 
 
 
-      $query = 'INSERT INTO "Person" (ssn, birthday, firstname, familyname, nation, title, gender, streetname, streetnumber, town, postalcode) VALUES (' . 
+      $query = ' BEGIN; INSERT INTO "Person" (ssn, birthday, firstname, familyname, nation, title, gender, streetname, streetnumber, town, postalcode) VALUES (' . 
             $ssn . ", ' " . $birthday . " ' ,'".$vorname . "','".$nachname."','".$nation."','".$title."','".$gender."','".$streetname."',".$streetnumber.",
             '".$town."',".$postalcode ."); ";
 
@@ -284,7 +302,7 @@ Sendemethode auf Post ändern
       
 
       if($_POST["person"] == "patient"){
-        $query = $query . 'INSERT INTO "Patient" (personssn, condition) VALUES (' . $ssn . ",'".$condition . "')";
+        $query = $query . 'INSERT INTO "Patient" (personssn, condition) VALUES (' . $ssn . ",'".$condition . "');";
         //$patientResult = pg_query($patientQuery) or die('Abfrage fehlgeschlagen: ' . pg_last_error());  
       }
       elseif ($_POST["person"] == "nurse") {
@@ -306,6 +324,7 @@ Sendemethode auf Post ändern
         //$doctorResult = pg_query($doctorQuery) or die('Abfrage fehlgeschlagen: ' . pg_last_error()); 
       }
 
+      $query = $query . "COMMIT;";
       //echo $query;
       $result = pg_query($query) or die('Abfrage fehlgeschlagen: ' . pg_last_error());
 
