f1af75403641e83de5ebcf87627599327ed0737e
Andi <andreas.stadler@indiekind.com>
2015-06-13 20:22:34 +0200

    +

--- delete.php
+++ delete.php
@@ -1,13 +1,13 @@
 <?php
 $ssn = $_GET['ssn'];
-$sql = "delete from \"Doctortreatingpatient\" where doctorssn = '$ssn';";
+$sql = "BEGIN; delete from \"Doctortreatingpatient\" where doctorssn = '$ssn';";
 $sql .= "delete from \"Doctortreatingpatient\" where patientssn = '$ssn';";
 $sql .= "delete from \"Patient\" where personssn = '$ssn';";
 $sql .= "delete from \"Doctor\" where staffssn = '$ssn';";
 $sql .= "delete from \"Nursepermissionto\" where nursessn = '$ssn';";
 $sql .= "delete from \"Nurse\" where staffssn = '$ssn';";
 $sql .= "delete from \"Staff\" where personssn = '$ssn';";
-$sql .= "delete from \"Person\" where ssn = '$ssn';";
+$sql .= "delete from \"Person\" where ssn = '$ssn'; COMMIT;";
 
     $dbconn = pg_connect("host=localhost dbname=postgres user=wlehner password=earoophojaej port=10000")
     or die('Verbindungsaufbau fehlgeschlagen: ' . pg_last_error());
